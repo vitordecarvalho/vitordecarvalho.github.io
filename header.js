3202c47e522841a973a4f0e74f11317e3657e06a
vitor <vitordecarvalho@gmail.com>
2018-01-18 20:40:07 -0800

    first page

---
+++ header.js
@@ -0,0 +1,7 @@
+var cHeader = '<IMG SRC="Vitor_Carvalho.jpg" ALT="Vitor Carvalho" HEIGHT="170" ALIGN=LEFT> \
+          <h1>Vitor Carvalho</h1> \
+          <h2>Carnegie Mellon University</h2> \
+          <h3>Language Technologies Institute</h3> \
+	<h2>vitor<sub><IMG SRC="at.jpg" height=15></sub>cs.cmu.edu</h2>\
+'
+document.write(cHeader)
